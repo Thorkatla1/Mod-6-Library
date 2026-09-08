d417d9799ecdfbc693471dd5d23e79e579a1d026
Thorkatla1 <thorkatla@yahoo.com>
2026-09-08 14:04:34 -0400

    adding icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-
+import LibraryLogo from "../assets/Library.svg";
 const Nav = () => {
   return (
     <nav>
       <div className="nav__container">
         <a href="/">
-          <img src="" alt="" className="logo" />
+          <img src={LibraryLogo} alt="Library Logo" className="logo" />
         </a>
         <ul className="nav__links">
-          <li className="nav__list">
+          <li className="nav__link">
             <a href="/">Home</a>
           </li>
-          <li className="nav__list">
+          <li className="nav__link">
             <a href="/">Books</a>
           </li>
           <button className="btn__menu">
             <FontAwesomeIcon icon={faBars} />
           </button>
+          <li className="nav__icon">
+            <a href="/cart" className="nav__link">
+              <FontAwesomeIcon icon='faShoppingCart' />
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBars);
+library.add(faBars, faShoppingCart);
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 reportWebVitals();
